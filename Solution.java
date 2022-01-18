82b256493802b14a81e0dbc70cab0e958dacd774
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2022-01-18 22:17:29 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,17 @@
+static int getRightMostSetBit(int n) 
+    {
+        if(n == 0)
+          return 0;
+      
+        return (int)((Math.log10(n & -n)) / 
+                     Math.log10(2)) + 1;
+    }
+    
+    // Function to find the position of
+    // rightmost different bit in the
+    // binary representations of 'm' and 'n'
+    static int posOfRightMostDiffBit(int m, int n) 
+    {
+        // position of rightmost different bit
+        return getRightMostSetBit(m ^ n);
+    }
